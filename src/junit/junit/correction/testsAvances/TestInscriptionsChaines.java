14e68a7d40cb11b502816512146a434667a6d542
HugoB2409 <46574029+HugoB2409@users.noreply.github.com>
2022-04-12 22:20:31 -0400

    test unitaire

--- src/junit/junit/correction/testsAvances/TestInscriptionsChaines.java
+++ src/junit/junit/correction/testsAvances/TestInscriptionsChaines.java
@@ -12,64 +12,64 @@ import secretariat.TableauPrincipal;
 
 public class TestInscriptionsChaines extends BaseTestInscriptions {
 
-	/**
-	 * Vérifie que les itérateurs suivent toujours le chaînage.
-	 * 
-	 * @author Louis Hamel
-	 */
-	public static void assertChainedInscription(TableauPrincipal tab) {
+    /**
+     * Vérifie que les itérateurs suivent toujours le chaînage.
+     *
+     * @author Louis Hamel
+     */
+    public static void assertChainedInscription(TableauPrincipal tab) {
 
-		for (Etudiant std : tab.getEtudiants()) {
-			Iterator<Inscription> iter = std.iterator();
+        for (Etudiant std : tab.getEtudiants()) {
+            Iterator<Inscription> iter = std.iterator();
 
-			Inscription previous = null, current = null;
-			while (iter.hasNext()) {
-				previous = current;
-				current = iter.next();
+            Inscription previous = null, current = null;
+            while (iter.hasNext()) {
+                previous = current;
+                current = iter.next();
 
-				if (previous == null)
-					continue;
+                if (previous == null)
+                    continue;
 
-				//On vérifie que la prochaine inscription de cours de l'étudiant correspond au prochain dans la chaîne
-				assertSame(previous.getProchainCours(), current);
-			}
-			
-			if(previous != null)// Une autre fois pour le dernier
-				assertSame(previous.getProchainCours(), current);
+                //On vérifie que la prochaine inscription de cours de l'étudiant correspond au prochain dans la chaîne
+                assertTrue(previous.getProchainCours() == current);
+            }
 
-		}
-		
-		for (Cours crs : tab.getCours()) {
-			Iterator<Inscription> iter = crs.iterator();
+            if (previous != null)// Une autre fois pour le dernier
+                assertTrue(previous.getProchainCours() == current);
 
-			Inscription previous = null, current = null;
-			while (iter.hasNext()) {
-				previous = current;
-				current = iter.next();
+        }
 
-				if (previous == null)
-					continue;
+        for (Cours crs : tab.getCours()) {
+            Iterator<Inscription> iter = crs.iterator();
 
-				//On vérifie que la prochaine inscription d'étudiant de ce cours correspond au prochain dans la chaîne
-				assertSame(previous.getProchainEtudiant(), current);
-			}
-			
-			if(previous != null)// Une autre fois pour le dernier
-				assertSame(previous.getProchainEtudiant(), current);
+            Inscription previous = null, current = null;
+            while (iter.hasNext()) {
+                previous = current;
+                current = iter.next();
 
-		}
+                if (previous == null)
+                    continue;
 
-	}
-	
-	
-	/**
-	 * On ajoute la condition de chainage aux tests, qui seront refaits tels quels.
-	 */
-	@Override
-	protected void assertCoherence(TableauPrincipal tableau) {
-		super.assertCoherence(tableau);
-		
-		assertChainedInscription(tableau);
-	}
+                //On vérifie que la prochaine inscription d'étudiant de ce cours correspond au prochain dans la chaîne
+                assertTrue(previous.getProchainEtudiant() == current);
+            }
+
+            if (previous != null)// Une autre fois pour le dernier
+                assertTrue(previous.getProchainEtudiant() == current);
+
+        }
+
+    }
+
+
+    /**
+     * On ajoute la condition de chainage aux tests, qui seront refaits tels quels.
+     */
+    @Override
+    protected void assertCoherence(TableauPrincipal tableau) {
+        super.assertCoherence(tableau);
+
+        assertChainedInscription(tableau);
+    }
 
 }
